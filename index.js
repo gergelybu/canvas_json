f10b2684b7d8ad001acffa7572349febd5c6b2c6
gergelybu <118608660+gergelybu@users.noreply.github.com>
2023-06-16 17:37:14 +0200

    sword animation added
    attack command changes, does only one attack

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function processList(data) {
 function attack() {
   let jobs = "jobs.json";
   let monster = "monsters.json";
-  console.log("Auto-battle commencing!")
+  console.log("Battle commencing!");
 
   Promise.all([fetch(jobs), fetch(monster)])
     .then(function (responses) {
@@ -123,57 +123,113 @@ function attack() {
       let toDef = 0;
       let damage = 0;
       let critDmg = 0;
-      do {
-        toHit = Math.floor(Math.random() * 11) + fighterJobs[0].accuracy;
-        toDmg = Math.floor(Math.random() * 11) + fighterJobs[0].attack;
-        toCrit = Math.floor(Math.random() * 100) + fighterJobs[0].crit;
-        toDodge = Math.floor(Math.random() * 11) + goblinData[0].evasion;
-        toDef = Math.floor(Math.random() * 11) + goblinData[0].defense;
-        damage = toDmg - toDef + 1;
-        if (damage <= 0) {
-          damage = fighterJobs[0].attack;
+      //do {
+      toHit = Math.floor(Math.random() * 11) + fighterJobs[0].accuracy;
+      toDmg = Math.floor(Math.random() * 11) + fighterJobs[0].attack;
+      toCrit = Math.floor(Math.random() * 100) + fighterJobs[0].crit;
+      toDodge = Math.floor(Math.random() * 11) + goblinData[0].evasion;
+      toDef = Math.floor(Math.random() * 11) + goblinData[0].defense;
+      damage = toDmg - toDef + 1;
+      $("canvas").drawImage({
+        layer: true,
+        name: "sword",
+        source: "./char/sword_empty.png",
+        width: 50,
+        height: 75,
+        x: 300,
+        y: 100,
+        //rotate: 100,
+      });
+      $("canvas").animateLayer(
+        "sword",
+        {
+          x: 300,
+          y: 100,
+          width: 100,
+          height: 50,
+        },
+        1000,
+        function (layer) {
+          // Callback function
+          $(this).animateLayer(
+            layer,
+            {
+              x: 100,
+              y: 140,
+              rotate: 180,
+            },
+            "slow",
+            "swing"
+          );
         }
-        critDmg = (damage - toDef) * 2 + 1;
-        if (toCrit > 99) {
-          console.log(fighterJobs[0].job + " CRITICAL HIT!!!");
-          console.log(fighterJobs[0].job + " dealt: " + critDmg);
-          goblinData[0].current_hp -= critDmg;
-          console.log(goblinData[0].current_hp);
-        } else if (toHit >= toDodge) {
-          console.log(fighterJobs[0].job + " hit with the attack!");
-          console.log(fighterJobs[0].job + " dealt: " + damage);
-          goblinData[0].current_hp -= damage;
-          console.log(goblinData[0].current_hp);
-        } else {
-          console.log(fighterJobs[0].job + " missed the attack!");
-          console.log(goblinData[0].current_hp);
+      );
+      $("canvas").animateLayer(
+        "sword",
+        {
+          x: 300,
+          y: 100,
+          width: 100,
+          height: 50,
+        },
+        1000,
+        function (layer) {
+          $(this).animateLayer(
+            layer,
+            {
+              x: 300,
+              y: 100,
+              rotate: 360,
+            },
+            "slow",
+            "swing"
+          );
         }
+      );
 
-        toHit = Math.floor(Math.random() * 11) + goblinData[0].accuracy;
-        toDmg = Math.floor(Math.random() * 11) + goblinData[0].attack;
-        toCrit = Math.floor(Math.random() * 100) + goblinData[0].crit;
-        toDodge = Math.floor(Math.random() * 11) + fighterJobs[0].evasion;
-        toDef = Math.floor(Math.random() * 11) + fighterJobs[0].defense;
-        damage = toDmg - toDef + 1;
-        if (damage <= 0) {
-          damage = goblinData[0].attack;
-        }
-        critDmg = (damage - toDef) * 2 + 1;
-        if (toCrit > 99) {
-          console.log(goblinData[0].monster + " CRITICAL HIT!!!");
-          console.log(goblinData[0].monster + " dealt: " + critDmg);
-          fighterJobs[0].current_hp -= critDmg;
-          console.log(fighterJobs[0].current_hp);
-        } else if (toHit >= toDodge) {
-          console.log(goblinData[0].monster + " hit with the attack!");
-          console.log(goblinData[0].monster + " dealt: " + damage);
-          fighterJobs[0].current_hp -= damage;
-          console.log(fighterJobs[0].current_hp);
-        } else {
-          console.log(goblinData[0].monster + " missed the attack!");
-          console.log(fighterJobs[0].current_hp);
-        }
-      } while (fighterJobs[0].current_hp >= 0 && goblinData[0].current_hp >= 0);
+      if (damage <= 0) {
+        damage = fighterJobs[0].attack;
+      }
+      critDmg = (damage - toDef) * 2 + 1;
+      if (toCrit > 99) {
+        console.log(fighterJobs[0].job + " CRITICAL HIT!!!");
+        console.log(fighterJobs[0].job + " dealt: " + critDmg);
+        goblinData[0].current_hp -= critDmg;
+        console.log(goblinData[0].current_hp);
+      } else if (toHit >= toDodge) {
+        console.log(fighterJobs[0].job + " hit with the attack!");
+        console.log(fighterJobs[0].job + " dealt: " + damage);
+        goblinData[0].current_hp -= damage;
+        console.log(goblinData[0].current_hp);
+      } else {
+        console.log(fighterJobs[0].job + " missed the attack!");
+        console.log(goblinData[0].current_hp);
+      }
+
+      toHit = Math.floor(Math.random() * 11) + goblinData[0].accuracy;
+      toDmg = Math.floor(Math.random() * 11) + goblinData[0].attack;
+      toCrit = Math.floor(Math.random() * 100) + goblinData[0].crit;
+      toDodge = Math.floor(Math.random() * 11) + fighterJobs[0].evasion;
+      toDef = Math.floor(Math.random() * 11) + fighterJobs[0].defense;
+      damage = toDmg - toDef + 1;
+      if (damage <= 0) {
+        damage = goblinData[0].attack;
+      }
+      critDmg = (damage - toDef) * 2 + 1;
+      if (toCrit > 99) {
+        console.log(goblinData[0].monster + " CRITICAL HIT!!!");
+        console.log(goblinData[0].monster + " dealt: " + critDmg);
+        fighterJobs[0].current_hp -= critDmg;
+        console.log(fighterJobs[0].current_hp);
+      } else if (toHit >= toDodge) {
+        console.log(goblinData[0].monster + " hit with the attack!");
+        console.log(goblinData[0].monster + " dealt: " + damage);
+        fighterJobs[0].current_hp -= damage;
+        console.log(fighterJobs[0].current_hp);
+      } else {
+        console.log(goblinData[0].monster + " missed the attack!");
+        console.log(fighterJobs[0].current_hp);
+      }
+      //} while (fighterJobs[0].current_hp >= 0 && goblinData[0].current_hp >= 0);
       if (fighterJobs[0].current_hp > goblinData[0].current_hp) {
         console.log(fighterJobs[0].job + " won the fight!");
       } else {
